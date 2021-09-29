05c386d36414a6a7d29637097a99a7101131aaa0
jackson-stamper <jacksonstamper32@gmail.com>
2021-09-29 14:58:56 -0500

    first name inputs

--- LA6.appstudio/LA6.appstudio/forms/membership/membership.js
+++ LA6.appstudio/LA6.appstudio/forms/membership/membership.js
@@ -1,4 +1,38 @@
 /*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
+fName = prompt("What is your first name?")
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+if (members.includes(fName)){
+  console.log(`${fName}, you are already a member!`)
+}
+else{
+  members.push(fName)
+  console.log(`${fName}, you have been added to the membership roster`)
+}
+*/
+btnSubmission.onclick=function(){
+  let fName = inptName.value
+  let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+  let message = `${fName}, you are already a member!`
+  if (members.includes(fName)){
+  inptName.value = message
+  console.log(`${fName}, you are already a member!`)
+}
+else{
+  members.push(fName)
+  txtArea.value = `${fName}, you have been added to the membership roster`
+  console.log(`${fName}, you have been added to the membership roster`)
+}
+}
+
+
+/*
+  let userAddress = inptAddress.value
+  let userCity = inptCity.value
+
+  lblUserAddress.value = "Your address is" :<br>" + userAddress + "<br>"> + userCity
+                          ${userAddress}
+                          ${userCity}`
+}
+*/
